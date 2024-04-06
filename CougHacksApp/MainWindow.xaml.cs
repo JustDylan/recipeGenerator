cfaa204da188c9f403f4f35e10326b4f835d20ed
Huy Ky <huyky26@gmail.com>
2024-04-05 22:59:50 -0700

    Add event handler

--- CougHacksApp/MainWindow.xaml.cs
+++ CougHacksApp/MainWindow.xaml.cs
@@ -1,27 +1,11 @@
 ﻿using CougHacksApp.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Input;
-using Microsoft.Msagl.Core.Geometry.Curves;
-using Microsoft.Msagl.Core.Routing;
 //using Microsoft.Msagl;
 using Microsoft.Msagl.WpfGraphControl;
-using Microsoft.Win32;
-using Color = Microsoft.Msagl.Drawing.Color;
 //using LabelPlacement = Microsoft.Msagl.Core.Layout.LabelPlacement;
-using ModifierKeys = System.Windows.Input.ModifierKeys;
-using Size = System.Windows.Size;
 using Microsoft.Msagl.Drawing;
-using Microsoft.Msagl.Layout.Layered;
-using static Microsoft.Msagl.Core.Layout.LgNodeInfo;
-using Orientation = System.Windows.Controls.Orientation;
 
 namespace CougHacksApp
 {
@@ -46,6 +30,17 @@ namespace CougHacksApp
             //this.Show();
             //AutomaticGraphLayoutControl test = new AutomaticGraphLayoutControl();
             //this.CreateGraph(null, null);
+        }
+
+        public void Graph_Changed(object sender, EventArgs e)
+        {
+            if (sender is Recipe rec)
+            {
+                RecipeViewModel recipeVM = new RecipeViewModel(rec);
+                RecipeView recipeView = new RecipeView(recipeVM);
+                recipeView.Show();
+
+            }
         }
 
         public void CreateGraph(object sender, ExecutedRoutedEventArgs ex)
