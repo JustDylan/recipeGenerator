a63f3926f1fb3be34202fab3fba4cb5216a59e23
Huy Ky <huyky26@gmail.com>
2024-04-05 22:30:43 -0700

    RecipeView test

---
+++ CougHacksApp/View/RecipeView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using CougHacksApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CougHacksApp
+{
+    /// <summary>
+    /// Interaction logic for RecipeView.xaml
+    /// </summary>
+    public partial class RecipeView : Window
+    {
+        private RecipeViewModel recipe;
+        public RecipeView(RecipeViewModel recipe)
+        {
+            InitializeComponent();
+            this.recipe = recipe;
+            this.DataContext = recipe;
+        }
+
+
+
+    }
+}
---
+++ CougHacksApp/ViewModel/RecipeViewModel.cs
@@ -0,0 +1,88 @@
+﻿using CougHacksApp.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CougHacksApp.ViewModel
+{
+    
+    public class RecipeViewModel : ViewModelBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set 
+            { 
+                name = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        private string recipeList;
+
+        public string RecipeList
+        {
+            get { return recipeList; }
+            set 
+            { 
+                recipeList = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+
+        private List<string> ingredients;
+
+        public List<string> Ingredients
+        {
+            get 
+            { 
+                return ingredients;
+            }
+            set 
+            { 
+                ingredients = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string linkURL;
+        public string LinkURL
+        {
+            get
+            {
+                return linkURL;
+            }
+            set 
+            {
+                linkURL = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Recipe broadcaster.
+        /// </summary>
+        /// <param name="ingredients">List of ingredients (with quantity).</param>
+        /// <param name="linkURL">URL link</param>
+        public RecipeViewModel(string name, List<string> ingredients, string linkURL)
+        {
+            this.Name = name;
+            this.linkURL = linkURL;
+            this.ingredients = ingredients;
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append($"{this.Name} recipe:");
+            stringBuilder.AppendLine();
+            foreach(string ingredient in ingredients)
+            {
+                stringBuilder.Append($"- {ingredient}");
+            }
+        }
+    }
+}
