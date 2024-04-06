648a42083a6dff364a4caa164920f5e57a732c40
Huy Ky <huyky26@gmail.com>
2024-04-06 10:28:55 -0700

    Fix database

--- CougHacksApp/Model/FoodItemQueryManager.cs
+++ CougHacksApp/Model/FoodItemQueryManager.cs
@@ -1,12 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
-using Npgsql;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Documents;
+﻿using Npgsql;
 
 namespace CougHacksApp.Model
 {
@@ -23,25 +15,21 @@ namespace CougHacksApp.Model
             var connectionString = "Host=localhost; Username=postgres; Password=1004; Database=recipedatabase";
             await using var dataSource = NpgsqlDataSource.Create(connectionString);
 
-            // Updated SQL query to only include items that start with the input
-            // Corrected ILIKE pattern to use parameter placeholder correctly
+            // SQL query that selects from common_foods only
             string sql = @"
-            SELECT DISTINCT foodname 
-            FROM common_foods
-            WHERE foodname ILIKE @input || '%';"; // Items that start with the input
+            SELECT foodname FROM common_foods 
+            WHERE foodname ILIKE @input || '%'
+            ORDER BY foodname
+            LIMIT 10;";
 
             await using var command = dataSource.CreateCommand(sql);
-            // No need to call ToLower() if the database comparison is case-insensitive (ILIKE)
             command.Parameters.AddWithValue("@input", input);
 
             await using var reader = await command.ExecuteReaderAsync();
 
             while (await reader.ReadAsync())
             {
-                var foodItem = reader.GetString(0); // Assuming you want to maintain the original case from the database
-
-                // Since ILIKE is used for case-insensitive search and DISTINCT is used in SQL,
-                // the check for existing items in the list can be skipped
+                var foodItem = reader.GetString(0); // Get the food item name
                 foodItems.Add(foodItem);
             }
 
