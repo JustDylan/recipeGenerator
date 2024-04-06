ec8f72c2e5534b1289cdc26ffe130c7d5e9c9e27
Ross <rosskugler@gmail.com>
2024-04-05 21:43:44 -0700

    Added documentation to querymanagers

---
+++ RecipeQueryEngine/Recipe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeQueryEngine
+{
+    public class Recipe
+    {
+        public string Label { get; set; }
+        public List<String> FoodItems { get; set; }
+        public List<string> Ingredients { get; set; }
+        public string ImageUrl { get; set; }
+        public string Url { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine($"Recipe: {Label}");
+            Console.WriteLine("FoodItems:");
+            foreach (var ingredient in FoodItems)
+            {
+                Console.WriteLine($"- {ingredient}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
