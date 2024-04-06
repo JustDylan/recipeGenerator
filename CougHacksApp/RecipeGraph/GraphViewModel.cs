290f8fa5ea84b6acf6c622bc7448c76ef61351a3
Dylan Gyori <gyoridylan@gmail.com>
2024-04-06 01:53:48 -0700

    semi implemented graph factor and view model.

--- CougHacksApp/RecipeGraph/GraphViewModel.cs
+++ CougHacksApp/RecipeGraph/GraphViewModel.cs
@@ -1,10 +1,32 @@
-﻿using CougHacksApp.MVVM;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CougHacksApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using Microsoft.Msagl.Core.Geometry.Curves;
+using Microsoft.Msagl.Core.Routing;
+//using Microsoft.Msagl;
+using Microsoft.Msagl.WpfGraphControl;
+using Microsoft.Win32;
+using Color = Microsoft.Msagl.Drawing.Color;
+//using LabelPlacement = Microsoft.Msagl.Core.Layout.LabelPlacement;
+using ModifierKeys = System.Windows.Input.ModifierKeys;
+using Size = System.Windows.Size;
+using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.Layout.Layered;
+using static Microsoft.Msagl.Core.Layout.LgNodeInfo;
+using Orientation = System.Windows.Controls.Orientation;
 
 namespace CougHacksApp.RecipeGraph
 {
@@ -12,9 +34,31 @@ namespace CougHacksApp.RecipeGraph
     {
         public event EventHandler? GraphNodeChanged;
 
-        public GraphViewModel()
+        private Graph graph;
+
+        public GraphViewModel(Graph graph, List<Node> graphNodes)
         {
-        
+            GraphNodeChanged = delegate { };
+            this.graph = graph;
+            foreach (Node node in graphNodes)
+            {
+                node.Attr.VisualsChanged += (o, e) => { this.OnGraphNodeChange(o, e, node); };
+            }
+        }
+
+        /// <summary>
+        /// assigns this graph to the graph viewer
+        /// </summary>
+        /// <param name="graphViewer"></param>
+        public void AssignGraph(GraphViewer graphViewer)
+        {
+            graphViewer.Graph = this.graph;
+        }
+
+        private void OnGraphNodeChange(object? o, EventArgs e, Node node)
+        {
+            if(this.GraphNodeChanged != null)
+                this.GraphNodeChanged(node.UserData, e);
         }
     }
 }
