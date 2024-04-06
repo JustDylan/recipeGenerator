34a8b4616ccc0e341b6ad74470d3152a627b5f1d
suspiciouslyBee <suspiciously@bee.movie>
2024-04-06 01:50:48 -0700

    + Added Recipe List Window

---
+++ CougHacksApp/View/RecipeListView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using CougHacksApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CougHacksApp.View
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class RecipeListView : Window
+    {
+        private List<RecipeViewModel> RecipeList;
+        public RecipeListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
